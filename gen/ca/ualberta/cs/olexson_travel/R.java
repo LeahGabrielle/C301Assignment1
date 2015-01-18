ccf2892cbaa160ae6cb41db62ff0bdb5c0016a78
Leah Olexson <Leah@Leahs-MacBook-Pro.local>
2015-01-18 14:49:04 -0700

    Added item_list.xml

--- gen/ca/ualberta/cs/olexson_travel/R.java
+++ gen/ca/ualberta/cs/olexson_travel/R.java
@@ -26,18 +26,27 @@ public final class R {
     }
     public static final class id {
         public static final int ItemstextView=0x7f080005;
-        public static final int action_settings=0x7f080008;
+        public static final int action_settings=0x7f080010;
+        public static final int claim_editbutton=0x7f08000b;
+        public static final int claim_itemlisttextView=0x7f080008;
         public static final int claimlistView=0x7f080001;
+        public static final int claimstatustextView=0x7f08000a;
         public static final int claimtextView=0x7f080000;
+        public static final int costcurrency_alltextView=0x7f08000e;
+        public static final int daterangetextView=0x7f080009;
         public static final int edititembutton=0x7f080003;
+        public static final int emailbutton=0x7f08000d;
         public static final int item_categorytextView=0x7f080007;
         public static final int item_costcurrencytextView=0x7f080006;
         public static final int item_datetextView=0x7f080004;
+        public static final int listView1=0x7f08000f;
         public static final int newclaimbutton=0x7f080002;
+        public static final int newitem_button=0x7f08000c;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int expense_items=0x7f030001;
+        public static final int item_list=0x7f030002;
     }
     public static final class menu {
         public static final int main=0x7f070000;
