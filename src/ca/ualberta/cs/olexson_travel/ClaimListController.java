ebafaeb242e306f8113d4c60f342d047cb019a2f
LeahGabrielle <olexson@ualberta.ca>
2015-01-27 13:57:57 -0700

    put some stuff in ClaimListController, DOES NOT WORK YET

--- src/ca/ualberta/cs/olexson_travel/ClaimListController.java
+++ src/ca/ualberta/cs/olexson_travel/ClaimListController.java
@@ -1,5 +1,28 @@
 package ca.ualberta.cs.olexson_travel;
 
-public class ClaimListController {
+import java.io.IOException;
 
+public class ClaimListController {
+	
+	private static ClaimList claimlist = null;
+	static public ClaimList getClaimList() {
+		if (claimlist ==null){
+			try{
+				claimlist = ClaimListManager.getManager().loadClaimlist();
+				claimlist.addListener(new Listener()){
+					@Override
+					public void update(){
+						saveClaimList();
+					}
+				});
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			throw new RuntimeException ("Could not deserialize ClaimList from ClaimListManager");	
+		} catch (IOException e) {
+			e.printStackTrace();
+			throw new RuntimeException("Could not deserialize ClaimList from ClaimListManager");
+		}
+		}
+	}
+	
 }
