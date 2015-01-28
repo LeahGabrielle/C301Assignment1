f8ae4ccf3bdfa33ebb05e67083490ff1dd8e21ee
LeahGabrielle <olexson@ualberta.ca>
2015-01-28 12:32:49 -0700

    finished claimlistcontroller

--- src/ca/ualberta/cs/olexson_travel/ClaimListController.java
+++ src/ca/ualberta/cs/olexson_travel/ClaimListController.java
@@ -5,24 +5,40 @@ import java.io.IOException;
 public class ClaimListController {
 	
 	private static ClaimList claimlist = null;
+	//Warning: throws runTimeException
 	static public ClaimList getClaimList() {
 		if (claimlist ==null){
 			try{
 				claimlist = ClaimListManager.getManager().loadClaimlist();
-				claimlist.addListener(new Listener()){
+				claimlist.addListener(new Listener() {
 					@Override
 					public void update(){
 						saveClaimList();
 					}
 				});
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-			throw new RuntimeException ("Could not deserialize ClaimList from ClaimListManager");	
+			} catch (ClassNotFoundException e) {
+				e.printStackTrace();
+				throw new RuntimeException ("Could not deserialize ClaimList from ClaimListManager");	
+			} catch (IOException e) {
+				e.printStackTrace();
+				throw new RuntimeException("Could not deserialize ClaimList from ClaimListManager");
+			}
+		}
+		return claimlist;
+	}
+	static public void saveClaimList() {
+		try {
+			ClaimListManager.getManager().saveClaimList(getClaimList());
 		} catch (IOException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 			throw new RuntimeException("Could not deserialize ClaimList from ClaimListManager");
 		}
-		}
 	}
-	
+	public Claim chooseClaim() throws EmptyClaimListException {
+		return getClaimList().chooseClaim();
+	}
+	public void addClaim(Claim claim) {
+		getClaimList().addClaim(claim);
+	}
 }
