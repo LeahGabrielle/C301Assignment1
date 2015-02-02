c0a6513dfd7dd909d63e99a50f3bd873491ff95d
LeahGabrielle <olexson@ualberta.ca>
2015-02-02 12:16:52 -0700

    Update AmountCurrency.java

--- src/ca/ualberta/cs/olexson_travel/AmountCurrency.java
+++ src/ca/ualberta/cs/olexson_travel/AmountCurrency.java
@@ -1,4 +1,4 @@
-    //Copyright (C) 2015 Leah Olexsonn
+    //Copyright (C) 2015 Leah Olexson
 
     //This program is free software; you can redistribute it and/or modify
     //it under the terms of the GNU General Public License as published by
